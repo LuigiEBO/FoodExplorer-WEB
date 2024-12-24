a8ddf683a1a27cf00545ed7f5e1793a0bd7f2c80
luigi <luigigames2903@gmail.com>
2024-12-24 16:21:58 -0300

    definindo configurações

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+import GlobalStyles from "./styles/global"
+import { SignIn } from './pages/signIn'
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <SignIn/>
+    </ThemeProvider>
+  </StrictMode>
 )
