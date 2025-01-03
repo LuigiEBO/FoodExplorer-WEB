f7fedbf8f9b72ca8af04931632345f9f55e9e10b
luigi <luigigames2903@gmail.com>
2025-01-03 20:34:39 -0300

    colocando o token no cabeçalho das requisições

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import { Routes } from './routes'
+import { AuthProvider } from './hooks/auth.jsx'
 import theme from './styles/theme'
 import GlobalStyles from "./styles/global"
-import { SignIn } from './pages/signIn'
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles/>
-      <Routes/>
+      <GlobalStyles />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>
 )
--- src/pages/signIn/index.jsx
+++ src/pages/signIn/index.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react'
 import {Container, Logo, InputArea} from './style'
 import explorerImg from "../../assets/Polygon.svg"
 import { Input } from '../../Components/input'
 import { Button } from '../../Components/button'
 import { Link } from 'react-router-dom'
+import { useAuth } from '../../hooks/auth'
 export function SignIn () {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const {SignIn} = useAuth()
+
+  function handleSignIn() {
+    SignIn({email, password})
+  }
   return (
     <Container>
       <Logo>
@@ -11,16 +20,26 @@ export function SignIn () {
         <h1>Food Explorer</h1>
       </Logo>
       <InputArea>
-      <h1>Faça Login</h1>
+        <h1>Faça Login</h1>
         <div>
           <span>Email</span>
-          <Input placeholder="exemplo@gmail.com" />
+          <Input
+            placeholder="exemplo@gmail.com"
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
+          />
         </div>
         <div>
           <span>Senha</span>
-          <Input placeholder="No mínimo 6 caracteres" />
+          <Input
+            placeholder="No mínimo 6 caracteres"
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+          />
         </div>
-        <Button title="Entrar"></Button>
+        <Button title="Entrar" onClick={handleSignIn}></Button>
         <Link to="/register"> Crie uma Conta</Link>
       </InputArea>
     </Container>
