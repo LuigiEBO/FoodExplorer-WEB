85ab8241b537a7087ffdf421fbe4a7a9e3d85e76
luigi <luigigames2903@gmail.com>
2025-01-03 20:53:07 -0300

    adiconando a função de aumentar o número de pedido no foodArea

--- src/Components/foodArea/index.jsx
+++ src/Components/foodArea/index.jsx
@@ -1,18 +1,21 @@
+import {useState} from "react"
 import { Container } from "./style";
 import {FiMinus, FiPlus, } from "react-icons/fi"
 import { Button } from "../button";
 export function FoodArea ({icon: Icon, img, name, price}) {
-  return( 
-  <Container>
-    {Icon && <Icon size={20} />}
-    <img src={img} alt="" />
-    <h3>{name}</h3>
-    <span>{price}</span>
-    <div className="amount">
-      <FiMinus/>
-        <span>01</span>
-      <FiPlus/>
-    </div>
-    <Button title="Incluir"/>
-    </Container>)
+  const [amount, setAmount] = useState(1)
+  return (
+    <Container>
+      {Icon && <Icon size={20} />}
+      <img src={img} alt="" />
+      <h3>{name}</h3>
+      <span>{price}</span>
+      <div className="amount">
+        <FiMinus onClick={() => setAmount(amount - 1)} />
+        <span>{amount}</span>
+        <FiPlus onClick={() => setAmount(amount + 1)} />
+      </div>
+      <Button title="Incluir" />
+    </Container>
+  )
 }
