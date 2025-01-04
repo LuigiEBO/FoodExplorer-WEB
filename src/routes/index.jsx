d19c3cc9a0fdc40ab33808503a5f3441d2a6e64d
luigi <luigigames2903@gmail.com>
2025-01-03 21:07:49 -0300

    redirecionando as rotas

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,24 @@
 import { BrowserRouter } from "react-router-dom";
 import { AuthRoutes } from "./auth.routes";
+import { useAuth } from "../hooks/auth";
 import { CustomerRoutes } from "./customer.routes";
 import { AdminRoutes } from "./admin.routes";
 export function Routes() {
+  const {user} =  useAuth()
+
+  function SelectRoute() {
+    switch(user.role) {
+      case "admin":
+      return <AdminRoutes/>;
+      case "customer":
+      return <CustomerRoutes/>;
+      default: 
+      return <CustomerRoutes/>
+    }
+  }
   return (
     <BrowserRouter>
-      <AuthRoutes/>
+      {user? <SelectRoute/> : <AuthRoutes/>}
     </BrowserRouter>
   )
 }
